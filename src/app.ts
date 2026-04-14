a275e954d97f2aacf4e667afa964bc337b839786
C1347547 Pedro Esli Vieira Do Nascimento <c1347547@interno.bb.com.br>
2026-04-13 21:44:22 -0300

    feat: added metrics endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import express from "express";
 import { StorageUnavailableError } from "./errors/storage-unavailable-error";
 import tasksRouter from "./routes/tasks.routes";
+import client from "prom-client";
 
 const app = express();
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
 
 app.use(express.json());
 
 app.get("/health", (_req, res) => {
   res.status(200).json({ status: "ok" });
+});
+
+app.get("/metrics", async (_req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
 });
 
 app.use("/tasks", tasksRouter);
